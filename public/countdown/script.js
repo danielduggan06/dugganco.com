3aef2ca8ac2b6ae20efb3ca24523544b47b8b74c
Daniel Duggan <61025839+danielduggan06@users.noreply.github.com>
2023-11-14 14:50:15 -0500

    Update 1 script.js
    - Should add functionality to the countdown
    
    Signed-off-by: Daniel Duggan <61025839+danielduggan06@users.noreply.github.com>

--- public/countdown/script.js
+++ public/countdown/script.js
@@ -1,14 +1,25 @@
-const newYears = '1 Jan 2021'
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secondsEl = document.getElementById('seconds')
+
+const thanksGiving = "November 23 2023";
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
+    const thanksgivingDate = new Date(thanksGiving);
+    var currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) * 1000;
+    const totalSeconds = (thanksgivingDate - currentDate) / 1000;
 
-    const days = Math.floor(seconds / 3600 / 24);
+    var days = Math.floor(totalSeconds / 3600 / 24);
+    var hours = Math.floor(totalSeconds / 3600) % 24;
+    var mins = Math.floor(totalSeconds / 60) % 60;
+    var seconds = Math.floor(totalSeconds) % 60;
 
-        console.log(newYearsDate - currentDate);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
 }
 
 //initial call
